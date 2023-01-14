1971a4ea0b2232124c1350ffd57c6c80fe774601
Leondeodas1 <leondeodas1@gmail.com>
2023-01-13 23:46:23 -0500

    upload

--- flask_app/static/javascript/news.js
+++ flask_app/static/javascript/news.js
@@ -6,18 +6,27 @@ function search(element) {
     console.log(element.value);
     currentUsername = element.value;
 }
-function makecodercard(data) {  
+function makecodercard(data) {
+    var arr = [1,2,3,4,5];
+    count=0
+    for(var i = 0; i <=data.articles; i++);{
+        count += i;{
+            console.log(count);
+        }
+    }
     var res = `<div id="char" style= "border-style: solid;height:100px;width:100%;border-radius: 5%;padding: 10px;">
                     <button"><a href="${data.articles[0].url}" >${data.articles[0].title}</a></button>
                     <p style="font-size: small;">Published At: ${data.articles[0].publishedAt}</p>
+                    <p style="font-size: small;">Published At: ${data.articles}</p>
                 </div>`
     return res;
 }
 
 
 async function show() {
-    var response = await fetch("https://newsapi.org/v2/everything?q=" + currentUsername + "&from=2023-01-08&language=en&apiKey=0375043b8cca4d68a40e18b5f6df8f8b");
+    var response = await fetch("https://newsapi.org/v2/everything?q=" + currentUsername + "&from=2023-01-08&apiKey=0375043b8cca4d68a40e18b5f6df8f8b");
     var coderData = await response.json();
     console.log(coderData);
     thisis.innerHTML = makecodercard(coderData);
 }
+ 
